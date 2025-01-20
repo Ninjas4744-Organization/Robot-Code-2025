b129ad154b200cd241ceddb197c93a2512673c24
Eitan <ninjas4744@gmail.com>
2025-01-20 19:02:08 +0200

    Added Outtake Subsystem

---
+++ src/main/java/frc/robot/Constants/OuttakeConstants.java
@@ -0,0 +1,21 @@
+package frc.robot.Constants;
+
+import com.ninjas4744.NinjasLib.DataClasses.MainControllerConstants;
+import com.ninjas4744.NinjasLib.DataClasses.SimulatedControllerConstants;
+
+public class OuttakeConstants {
+    public static final MainControllerConstants kControllerConstants = new MainControllerConstants();
+    public static final SimulatedControllerConstants kSimulatedControllerConstants = new SimulatedControllerConstants();
+
+    static {
+        kControllerConstants.main.id = 30;
+        kControllerConstants.subsystemName = "Outtake";
+
+        kSimulatedControllerConstants.mainControllerConstants = kControllerConstants;
+        kSimulatedControllerConstants.motorType = SimulatedControllerConstants.MotorType.KRAKEN;
+    }
+
+    public static final double kOuttakeState = 1;
+    public static final double kIntakeState = 0.7;
+    public static final double kCLoseState = 0;
+}
