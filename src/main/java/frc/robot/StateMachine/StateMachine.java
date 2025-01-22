0abc994a5bfd8a0097afc17b30cf7af45bf3118e
shay <shaygorkov@gmail.com>
2025-01-22 21:10:30 +0200

    finished setEndConditionMap

--- src/main/java/frc/robot/StateMachine/StateMachine.java
+++ src/main/java/frc/robot/StateMachine/StateMachine.java
@@ -3,6 +3,7 @@ package frc.robot.StateMachine;
 import com.ninjas4744.NinjasLib.DataClasses.StateEndCondition;
 import com.ninjas4744.NinjasLib.StateMachineIO;
 import com.ninjas4744.NinjasLib.Swerve.SwerveController;
+import frc.robot.Subsystems.Elevator;
 import frc.robot.Subsystems.SwerveSubsystem;
 import edu.wpi.first.wpilibj.Timer;
 
@@ -12,6 +13,7 @@ public class StateMachine extends StateMachineIO<RobotStates> {
     }
 
     private Timer _outtakeTimer;
+    private Timer _hornTimer;
 
     @Override
     public boolean canChangeRobotState(RobotStates currentState, RobotStates wantedState) {
@@ -49,19 +51,27 @@ public class StateMachine extends StateMachineIO<RobotStates> {
 
     @Override
     protected void setEndConditionMap() {
-        addEndCondition(RobotStates.IDLE, new StateEndCondition<>(()-> true, RobotStates.CORAL_SEARCH));
+        addEndCondition(RobotStates.IDLE, new StateEndCondition<>(()-> RobotState.getInstance().isintakFinish(), RobotStates.CORAL_SEARCH));
+        addEndCondition(RobotStates.IDLE, new StateEndCondition<>(()-> RobotState.getInstance().isintakFinish(), RobotStates.CORAL_READY));
         addEndCondition(RobotStates.INTAKE, new StateEndCondition<>(()-> RobotState.getInstance().isintakFinish(), RobotStates.CORAL_READY));
         addEndCondition(RobotStates.L1, new StateEndCondition<>(() -> SwerveController.getInstance().isDriveAssistFinished(), RobotStates.GO_RIGHT_REEF));
         addEndCondition(RobotStates.L2, new StateEndCondition<>(() -> SwerveController.getInstance().isDriveAssistFinished(), RobotStates.GO_RIGHT_REEF));
         addEndCondition(RobotStates.L3, new StateEndCondition<>(() -> SwerveController.getInstance().isDriveAssistFinished(), RobotStates.GO_RIGHT_REEF));
         addEndCondition(RobotStates.L4, new StateEndCondition<>(() -> SwerveController.getInstance().isDriveAssistFinished(), RobotStates.GO_RIGHT_REEF));
-        addEndCondition(RobotStates.GO_RIGHT_REEF, new StateEndCondition<>(() -> SwerveSubsystem.getInstance().atReefSide() , RobotStates.OUTTAKE_READY));
-        addEndCondition(RobotStates.GO_LEFT_REEF, new StateEndCondition<>(() -> SwerveSubsystem.getInstance().atReefSide(), RobotStates.OUTTAKE_READY));
-        addEndCondition(RobotStates.OUTTAKE_READY, new StateEndCondition<>(()-> true, RobotStates.CORAL_SEARCH));
+        addEndCondition(RobotStates.GO_RIGHT_REEF, new StateEndCondition<>(() -> SwerveSubsystem.getInstance().atReefSide() && Elevator.getInstance().atGoal(), RobotStates.OUTTAKE_READY));
+        addEndCondition(RobotStates.GO_LEFT_REEF, new StateEndCondition<>(() -> SwerveSubsystem.getInstance().atReefSide() && Elevator.getInstance().atGoal(), RobotStates.OUTTAKE_READY));
+        addEndCondition(RobotStates.OUTTAKE_READY, new StateEndCondition<>(()-> _outtakeTimer.get()<1, RobotStates.CLOSE));
+        addEndCondition(RobotStates.OUTTAKE_READY, new StateEndCondition<>(()-> Elevator.getInstance().isResetted(), RobotStates.IDLE));
+        addEndCondition(RobotStates.REMOVE_ALGAE, new StateEndCondition<>(()-> _hornTimer.get()<1, RobotStates.IDLE));
     }
 
     @Override
     protected void setFunctionMaps() {
+        _outtakeTimer = new Timer();
+        _hornTimer = new Timer();
+
+        addFunctionToOnChangeMap( _outtakeTimer::restart, RobotStates.CLOSE, RobotStates.RESET);
+        addFunctionToOnChangeMap( _outtakeTimer::restart, RobotStates.IDLE, RobotStates.RESET);
 
     }
 }
